5e30d9423e5530d241ed9f9beecce9f2c7cc7cc5
Xytronix <loewen.michael.geschaeftlich@gmail.com>
2025-07-22 07:43:13 +0200

    Fix config

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
-    sveltekit({
-      compilerOptions: {
-        runes: true,
-      },
-    }),
+    sveltekit(),
     tailwindcss(),
   ],
   ...(process.env.VITEST && {
